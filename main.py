4afc0c63b5eb9c7beded0a4bd6c025da75d5a41b
phattymcfee <ramosa@berea.edu>
2023-02-08 09:43:19 -0500

    made two functions: new_function & reverse_a_word

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 # This is an example for branching!
 
 def main():
-    
+
     print("Y'all gon' learn today!")
 
 main()
+
+
+# def new_function():
+#     for i in range(1,11):
+#         print(i)
+#
+# new_function()
+
+# def reverse_a_word(word):
+#         reversed = word[::-1]
+#
+#         print(reversed)
+#
+# reverse_a_word("cat")
